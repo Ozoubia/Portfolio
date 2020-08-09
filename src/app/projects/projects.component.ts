79ff5e9cda974fd21c63e504212f5c240c29ea27
ozoubia <ozoubia@gmail.com>
2020-08-09 23:14:14 +0300

    the filter functionnality is working, only design is left

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import $ from 'jquery';
+import Isotope from 'isotope-layout';
+declare var $: $
+
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
-export class ProjectsComponent implements OnInit {
 
+export class ProjectsComponent implements OnInit {
+  
   constructor() { }
 
+
+
   ngOnInit(): void {
+    // selecting all buttons
+    let $btns = $('.buttons-group button');
+    
+    $btns.click(function(e){
+      $('.buttons-group button').removeClass('active');
+      e.target.classList.add('active');
+
+      let selector = $(e.target).attr('data-filter');
+      $('.grid').isotope({
+        filter: selector
+      })
+
+      return false;
+    })
+
   }
+
 
 }
